a094b3a047389b6e054676623a56f999b719b7e1
Josué Aquino <aquinojosue3@gmail.com>
2020-06-08 16:39:06 -0600

    Implementacion crud autor

---
+++ autor/actualizar.php
@@ -0,0 +1,17 @@
+<?php
+include("..\db.php");
+$respuesta = array('resultado' => 0);
+if (isset($_POST['elementoActualizar'])) {
+    $obj = json_decode($_POST["elementoActualizar"], false);
+
+    $stmt = $con->prepare("UPDATE autor SET nomAutor = ?, apeAutor = ? WHERE idautor = ?");
+    $stmt->bind_param('iii', $obj->nomAutor, $obj->apeAutor, $obj->idAutor);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 1) {
+        $respuesta = array('resultado' => 1);
+    }
+    $stmt->close();
+}
+echo json_encode($respuesta);
+$con->close();

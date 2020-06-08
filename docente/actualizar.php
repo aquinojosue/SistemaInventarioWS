41dd786acec7eb422fb81790248b621b482c482c
Josué Aquino <aquinojosue3@gmail.com>
2020-06-08 17:11:10 -0600

    Implementacion crud docente

---
+++ docente/actualizar.php
@@ -0,0 +1,17 @@
+<?php
+include("..\db.php");
+$respuesta = array('resultado' => 0);
+if (isset($_POST['elementoActualizar'])) {
+    $obj = json_decode($_POST["elementoActualizar"], false);
+
+    $stmt = $con->prepare("UPDATE docente SET docentes_nombre = ? WHERE docentes_id = ?");
+    $stmt->bind_param('si', $obj->docentes_nombre, $obj->docentes_id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 1) {
+        $respuesta = array('resultado' => 1);
+    }
+    $stmt->close();
+}
+echo json_encode($respuesta);
+$con->close();
---
+++ docente/eliminar.php
@@ -0,0 +1,16 @@
+<?php
+include("../db.php");
+$respuesta = array('resultado' => 0);
+if (isset($_POST['elementoEliminar'])) {
+    $obj = json_decode($_POST["elementoEliminar"], false);
+    $stmt = $con->prepare("DELETE FROM docente WHERE AND docentes_id = ?");
+    $stmt->bind_param("i", $obj->docentes_id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 1) {
+        $respuesta = array('resultado' => 1);
+    }
+    $stmt->close();
+    echo json_encode($respuesta);
+}
+$con->close();

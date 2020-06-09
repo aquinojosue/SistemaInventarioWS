d300d5bdcd653bf08226de32f5e1737884270665
aquinojosue <aquinojosue3@gmail.com>
2020-06-08 23:08:53 -0600

    implementacion del crud de detalledescargos

---
+++ detalledescargos/eliminar.php
@@ -0,0 +1,16 @@
+<?php
+include("../db.php");
+$respuesta = array('resultado' => 0);
+if (isset($_POST['elementoEliminar'])) {
+    $obj = json_decode($_POST["elementoEliminar"], false);
+    $stmt = $con->prepare("DELETE FROM detalledescargos WHERE equipo_id = ? AND descargo_id = ?");
+    $stmt->bind_param('iii', $obj->hora_id, $obj->equipo_id, $obj->descargo_id);
+    $stmt->execute();
+
+    if ($stmt->affected_rows == 1) {
+        $respuesta = array('resultado' => 1);
+    }
+    $stmt->close();
+    echo json_encode($respuesta);
+}
+$con->close();

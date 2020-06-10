6c7d759c83c14201b436150ebf3d9c396074735c
Josué Aquino <aquinojosue3@gmail.com>
2020-06-09 20:38:49 -0600

    utf-8

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
-    $host = "localhost";
-    $usuario = "phpmyadmin";
-    $password = "admin";
-    $base = "inventario";
-    $con = mysqli_connect($host, $usuario, $password, $base);
+mb_internal_encoding('UTF-8');
+mb_http_output('UTF-8');
+$host = "localhost";
+$usuario = "phpmyadmin";
+$password = "admin";
+$base = "inventario";
+$con = mysqli_connect($host, $usuario, $password, $base);
 
-    if(mysqli_connect_errno()){
-        echo "Error en la conexion de MySQL: ".mysqli_connect_error();
-        exit();
-    }
-?>
+if(mysqli_connect_errno()){
+    echo "Error en la conexion de MySQL: ".mysqli_connect_error();
+    exit();
+}
